2c8a1d97901469a4c8e0489f1dc5a9a15c4183ee
František Holubec <frantisek.holubec@gmail.com>
2020-05-17 20:00:50 +0200

    clear backstack on bottom nav change

--- app/src/main/java/com/example/alcoholcounter/MainActivity.kt
+++ app/src/main/java/com/example/alcoholcounter/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.alcoholcounter
 
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.alcoholcounter.ui.events.EventListFragment
+import com.example.alcoholcounter.ui.map.MapFragment
+import com.example.alcoholcounter.ui.statistics.StatisticsFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +25,22 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_map, R.id.navigation_statistics, R.id.navigation_events))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        navView.setOnNavigationItemSelectedListener {
+            var selectedFragment: Fragment? = null;
+            when (it.itemId) {
+                R.id.navigation_map -> selectedFragment = MapFragment()
+                R.id.navigation_statistics -> selectedFragment = StatisticsFragment()
+                R.id.navigation_events -> selectedFragment = EventListFragment()
+            }
+            val fragmentManager = supportFragmentManager
+            fragmentManager.popBackStackImmediate(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+            if (selectedFragment != null) {
+                val transaction = fragmentManager.beginTransaction()
+                transaction.replace(R.id.nav_host_fragment, selectedFragment)
+                transaction.commit()
+            }
+            true
+        }
     }
 
     fun replaceFragment(fragment: Fragment){
@@ -32,12 +50,4 @@ class MainActivity : AppCompatActivity() {
         transaction.addToBackStack(fragment.tag)
         transaction.commit()
     }
-}
-
-fun AppCompatActivity.replaceFragment(fragment: Fragment){
-    val fragmentManager = supportFragmentManager
-    val transaction = fragmentManager.beginTransaction()
-    transaction.replace(R.id.nav_host_fragment, fragment)
-    transaction.addToBackStack(fragment.tag)
-    transaction.commit()
 }
