3e8579f926fb7fdd3e562f2dc47193a229379769
Ales Paroulek <456555@mail.muni.cz>
2020-05-21 19:11:35 +0200

    added map

--- app/src/main/java/com/example/alcoholcounter/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/alcoholcounter/ui/map/MapFragment.kt
@@ -1,14 +1,39 @@
 package com.example.alcoholcounter.ui.map
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.example.alcoholcounter.R
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import kotlinx.android.synthetic.main.fragment_map.*
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(),
+                    OnMapReadyCallback,
+                    GoogleApiClient.ConnectionCallbacks,
+                    GoogleApiClient.OnConnectionFailedListener,
+                    LocationListener {
+
+    private var _map : GoogleMap? = null
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        map_view.onCreate(savedInstanceState)
+        map_view.onResume()
+        map_view.getMapAsync(this)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_map, container, false)
@@ -17,7 +42,36 @@ class MapFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val textView: TextView = view.findViewById(R.id.text_map)
-        textView.text = "Map"
+        //val textView: TextView = view.findViewById(R.id.text_map)
+        //textView.text = "Map"
     }
+
+    override fun onMapReady(map: GoogleMap?) {
+        map?.let { _map = it }
+
+/*
+        if (ActivityCompat.checkSelfPermission(this.requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+        }
+
+        _map!!.isMyLocationEnabled = true*/
+
+    }
+
+    override fun onConnected(p0: Bundle?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        TODO("Not yet implemented")
+    }
+
 }
