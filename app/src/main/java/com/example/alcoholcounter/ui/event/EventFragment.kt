b74af6f0f9f72f5bdb0b73af73f9e404f74b6434
František Holubec <frantisek.holubec@gmail.com>
2020-05-23 22:18:29 +0200

    Fd Up Rotation FINALY SoMeHOw WORKS

--- app/src/main/java/com/example/alcoholcounter/ui/event/EventFragment.kt
+++ app/src/main/java/com/example/alcoholcounter/ui/event/EventFragment.kt
@@ -1,26 +1,25 @@
 package com.example.alcoholcounter.ui.event
 
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.alcoholcounter.MainActivity
 import com.example.alcoholcounter.R
 import com.example.alcoholcounter.ui.events.Event
-import cz.pv239.seminar2.EventDB
 import kotlinx.android.synthetic.main.fragment_event.*
-import kotlinx.android.synthetic.main.fragment_event.fab
-import kotlinx.android.synthetic.main.fragment_eventlist.*
 import java.text.DateFormat
 import java.util.*
+
 
 class EventFragment(val event : Event) : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_event, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.event_menu, menu)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
